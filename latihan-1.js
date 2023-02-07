d21c54416adcbbc78c7e5c86ebdfe104bc29f442
MIMAALIJA <muhammadibnumuhammad20051003@gmail.com>
2023-02-07 19:16:26 +0700

    volume rentacle

---
+++ latihan-1.js
@@ -0,0 +1,7 @@
+// luas persegipanjang
+function persegipanjang(panjang, lebar) {
+    return panjang * lebar
+}
+
+let luas = persegipanjang(60, 15)
+console.log('Luas Persegi panjang = ' + luas)
---
+++ latihan-2.js
@@ -0,0 +1,16 @@
+//lebar luas volume persegipanjang
+
+function balok(panjang, lebar, tinggi){
+    return panjang * lebar * tinggi
+}
+let volume = balok(40, 20,15)
+console.log('Volume Balok = ' + volume)
+function volumeBalok(p,l,t){
+        return result = p * l * t;
+    }
+// function luaspermukaanbalok( p, l, t){
+//     return p * l + l * t + p * t
+// }
+let luaspermukaanbalok = luaspermukaanbalok(40, 20, 15)
+let hasil = luaspermukaanbalok * 2
+console.log('Luas Permukaan Balok = ' + hasil)
